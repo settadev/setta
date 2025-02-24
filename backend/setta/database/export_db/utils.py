f59c66ffe36a2e8aac40a4b5b7eb4a8960d8cba9
KevinMusgrave <kevin.musgrave@gmail.com>
2025-02-24 09:03:50 -0500

    export-db: don't export json sources

--- backend/setta/database/export_db/utils.py
+++ backend/setta/database/export_db/utils.py
@@ -10,6 +10,6 @@ from setta.utils.constants import SETTA_FILES_FOLDER
 def get_configs_and_root_folder(db, filename):
     # export defaultdict the same way as dict
     yaml.add_representer(defaultdict, Representer.represent_dict)
-    configs = load_full_project(db)
+    configs = load_full_project(db, do_load_json_sources=False)
     root_folder = SETTA_FILES_FOLDER / f"{filename}_export"
     return configs, root_folder
