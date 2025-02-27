48c33eb695eacd42207ebcb1493e44cf79340d03
KevinMusgrave <kevin.musgrave@gmail.com>
2025-02-27 10:33:58 -0500

    remove is_from_json_source

--- backend/setta/utils/constants.py
+++ backend/setta/utils/constants.py
@@ -88,10 +88,6 @@ def load_constants():
         DEFAULT_VALUES.update(json.load(f))
 
 
-def is_from_json_source(id):
-    return id and id.startswith(C.JSON_SOURCE_PREFIX)
-
-
 CODE_INFO_TABLE_DATA_JSON_FIELDS = set(
     (
         "editable",
