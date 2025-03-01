939a45d109b49fbc0bf1342ccb9b53a3f3a115f4
KevinMusgrave <kevin.musgrave@gmail.com>
2025-03-01 14:39:33 -0500

    add Hide/Show option in Selection context menu

--- frontend/src/components/Utils/ContextMenus/SelectionContextMenu.jsx
+++ frontend/src/components/Utils/ContextMenus/SelectionContextMenu.jsx
@@ -4,10 +4,12 @@ import { useReactFlow } from "forks/xyflow/core/store";
 import _ from "lodash";
 import { clearActiveSectionIds } from "state/actions/activeSections";
 import { closeAllContextMenus } from "state/actions/contextMenus";
+import { getSectionVisibilityKey } from "state/actions/sectionInfos";
 import { deleteSections } from "state/actions/sections/deleteSections";
 import { createGroup } from "state/actions/sections/groupSections";
 import { maybeIncrementProjectStateVersion } from "state/actions/undo";
 import { useContextMenus, useSectionInfos } from "state/definitions";
+import { VIEWING_EDITING_MODE } from "utils/constants";
 import { ContextMenuCore } from "./ContextMenuCore";
 
 export function SelectionContextMenu() {
@@ -16,9 +18,31 @@ export function SelectionContextMenu() {
     _.isEqual,
   );
 
-  const allLocked = useSectionInfos((x) =>
-    nodeIds.map((n) => x.x[n].positionAndSizeLocked).every((p) => p),
-  );
+  const { allVisible, allLocked, viewingEditingMode } = useSectionInfos((x) => {
+    const { viewingEditingMode } = x.projectConfig;
+    const visibilityKey = getSectionVisibilityKey(viewingEditingMode);
+    const nodeMap = x.x;
+
+    let allVisible = true;
+    let allLocked = true;
+
+    for (const id of nodeIds) {
+      const node = nodeMap[id];
+      if (!node.visibility[visibilityKey]) {
+        allVisible = false;
+      }
+
+      if (!node.positionAndSizeLocked) {
+        allLocked = false;
+      }
+
+      if (!allVisible && !allLocked) {
+        break;
+      }
+    }
+
+    return { allVisible, allLocked, viewingEditingMode };
+  });
 
   const closeContextMenu = () => {
     closeAllContextMenus();
@@ -51,9 +75,21 @@ export function SelectionContextMenu() {
     closeContextMenu();
   }
 
+  function onClickVisibility() {
+    useSectionInfos.setState((state) => {
+      for (const n of nodeIds) {
+        state.x[n].visibility[getSectionVisibilityKey(viewingEditingMode)] =
+          !allVisible;
+      }
+    });
+    closeContextMenu();
+  }
+
   const lockItem = allLocked
     ? "Unlock Position & Size"
     : "Lock Position & Size";
+
+  const visibilityItem = allVisible ? "Hide" : "Show";
 
   return (
     <ContextMenuCore
@@ -65,6 +101,9 @@ export function SelectionContextMenu() {
       <Item onClick={onClickCreate}>Create Group</Item>
       <Item onClick={onClickDelete}>Delete</Item>
       <Item onClick={onClickLock}>{lockItem}</Item>
+      {viewingEditingMode === VIEWING_EDITING_MODE.USER_EDIT && (
+        <Item onClick={onClickVisibility}>{visibilityItem}</Item>
+      )}
     </ContextMenuCore>
   );
 }
