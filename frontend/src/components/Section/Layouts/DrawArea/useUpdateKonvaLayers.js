ae69a4acee45ec5a249163e2a30190166ad3bbab
KevinMusgrave <kevin.musgrave@gmail.com>
2025-03-03 18:14:44 -0500

    move hook to useUpdateKonvaLayers

---
+++ frontend/src/components/Section/Layouts/DrawArea/useUpdateKonvaLayers.js
@@ -0,0 +1,45 @@
+import Konva from "konva";
+import { useEffect } from "react";
+
+export function useUpdateKonvaLayers({
+  stageRef,
+  layers,
+  konvaLayersRef,
+  layerLinesRef,
+  updateLayerCache,
+}) {
+  // Update Konva layers when React layers state changes
+  useEffect(() => {
+    if (!stageRef.current) return;
+
+    // Create new layers if needed
+    layers.forEach((layer) => {
+      if (!konvaLayersRef.current[layer.id]) {
+        const newKonvaLayer = new Konva.Layer();
+        stageRef.current.add(newKonvaLayer);
+        konvaLayersRef.current[layer.id] = newKonvaLayer;
+        layerLinesRef.current[layer.id] = [];
+      }
+
+      // Update visibility
+      konvaLayersRef.current[layer.id].visible(layer.visible);
+    });
+
+    // Remove deleted layers
+    Object.keys(konvaLayersRef.current).forEach((layerId) => {
+      const numLayerId = parseInt(layerId, 10);
+      if (!layers.some((l) => l.id === numLayerId)) {
+        konvaLayersRef.current[layerId].destroy();
+        delete konvaLayersRef.current[layerId];
+        delete layerLinesRef.current[layerId];
+      }
+    });
+
+    // Make sure the layers are in the correct order
+    layers.forEach((layer, index) => {
+      konvaLayersRef.current[layer.id].setZIndex(index);
+      // Update layer cache after reordering
+      updateLayerCache(layer.id);
+    });
+  }, [layers, updateLayerCache]);
+}
