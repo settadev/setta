ececbcb190892785ef992ba7a8900b6624b2d3af
Jeff Musgrave <jeff.musgrave@gmail.com>
2025-03-05 13:19:29 -0500

    fixed a few things

--- frontend/src/components/AppHeader/RunButton.jsx
+++ frontend/src/components/AppHeader/RunButton.jsx
@@ -12,7 +12,7 @@ export function RunButton() {
   const [isOpen, setIsOpen] = useState(false);
 
   const baseClasses =
-    "transition-colors text-xs font-semibold text-setta-700 dark:text-setta-100 py-1 px-2 rounded-md gap-1 select-none self-center flex items-center hover:bg-setta-100 dark:hover:bg-setta-800 bg-transparent active:!bg-blue-100 active:dark:!bg-blue-950";
+    "transition-colors text-xs font-semibold text-setta-700 dark:text-setta-100 h-full rounded-md gap-1 select-none flex items-center hover:bg-setta-100 dark:hover:bg-setta-800 bg-transparent active:!bg-blue-100 active:dark:!bg-blue-950";
 
   // Menu item styling with proper hover effect that's contained within the button
   const menuItemClasses =
@@ -28,13 +28,37 @@ export function RunButton() {
         setIsOpen(false);
       },
     },
+    {
+      label: "Run Interactive Functions",
+      icon: <RiPlayMiniFill />,
+      action: () => {
+        sendProjectToAllInteractiveCode();
+        setIsOpen(false);
+      },
+    },
+    {
+      label: "Run Interactive Functions Functions Functions Functions",
+      icon: <RiPlayMiniFill />,
+      action: () => {
+        sendProjectToAllInteractiveCode();
+        setIsOpen(false);
+      },
+    },
+    {
+      label: "Run",
+      icon: <RiPlayMiniFill />,
+      action: () => {
+        sendProjectToAllInteractiveCode();
+        setIsOpen(false);
+      },
+    },
   ];
 
   return (
-    <div className="flex items-center">
+    <div className="flex justify-stretch">
       <Button
         onClick={runOrImportAllCode}
-        twClasses={`${baseClasses} rounded-r-none`}
+        twClasses={`${baseClasses} rounded-r-none px-2`}
         {...getFloatingBoxHandlers({ content: "Run your code." })}
       >
         <RiPlayMiniFill />
@@ -43,13 +67,11 @@ export function RunButton() {
 
       <StandardPopover
         trigger={
-          <Button
-            twClasses={`${baseClasses} pl-1 pr-1 rounded-l-none border-l border-setta-200 dark:border-setta-700`}
-          >
+          <Button twClasses={`${baseClasses} px-1 rounded-l-none`}>
             <RiArrowDownSLine />
           </Button>
         }
-        contentClasses="bg-white dark:bg-setta-900 py-1 px-0 rounded-md shadow-lg z-50 min-w-36 w-48 border border-setta-200 dark:border-setta-800 overflow-hidden"
+        contentClasses="bg-white dark:bg-setta-900 py-1 rounded-md shadow-lg z-50 min-w-36 w-48 border border-setta-200 dark:border-setta-800 overflow-hidden"
         open={isOpen}
         onOpenChange={setIsOpen}
         arrowClasses="hidden"
@@ -60,7 +82,8 @@ export function RunButton() {
             className={menuItemClasses}
             onClick={action.action}
           >
-            {action.icon}
+            <i className="w-2">{action.icon}</i>
+
             <span className="ml-0.5">{action.label}</span>
           </button>
         ))}
