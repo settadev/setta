aa019324cc00f46afd3487a8555db89231d64439
KevinMusgrave <kevin.musgrave@gmail.com>
2025-03-05 16:26:47 -0500

    update vite.config.js, tailwind.config.js, and package.json to avoid crashing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,8 @@
-/// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgrPlugin from "vite-plugin-svgr";
 import viteTsconfigPaths from "vite-tsconfig-paths";
-import type { UserConfig as VitestUserConfigInterface } from "vitest/config";
 import constants from "../constants/constants.json";
-
-const vitestConfig: VitestUserConfigInterface = {
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
-  },
-};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,5 +15,4 @@ export default defineConfig({
   preview: {
     port: constants.DEV_MODE_FRONTEND_PORT,
   },
-  test: vitestConfig.test,
 });
