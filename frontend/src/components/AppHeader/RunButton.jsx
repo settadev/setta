ac4f3bd2b6ca7bc2a66cb3746dc3190b875f6e38
Jeff Musgrave <jeff.musgrave@gmail.com>
2025-03-06 16:36:54 -0500

    run button dropdown is now a menu not a popover. project search now squeezes into smaller space. notifications now have an extra icon and are a self contained div

--- frontend/src/components/AppHeader/RunButton.jsx
+++ frontend/src/components/AppHeader/RunButton.jsx
@@ -1,5 +1,6 @@
 import { Button } from "components/Utils/atoms/buttons/Button";
-import { StandardPopover } from "components/Utils/atoms/popover/standardpopover";
+import { NavbarMenuDropdown } from "components/Utils/atoms/menubar/menudropdown";
+import { MenuItem } from "components/Utils/atoms/menubar/menudropdownitem";
 import { getFloatingBoxHandlers } from "components/Utils/FloatingBox";
 import { useState } from "react";
 import { RiArrowDownSLine, RiPlayMiniFill } from "react-icons/ri";
@@ -40,8 +41,24 @@ export function RunButton() {
         <RiPlayMiniFill />
         Run
       </Button>
+      <NavbarMenuDropdown
+        triggerClassName="nodrag flex cursor-pointer select-none items-center justify-between gap-1 rounded-r-md py-1 px-1 text-xs font-semibold leading-none text-setta-700 outline-none transition-colors duration-150 hover:bg-setta-100 focus-visible:ring-2 dark:text-setta-200 dark:hover:bg-setta-800"
+        trigger={<RiArrowDownSLine />}
+      >
+        {runActions.map((action, index) => (
+          <MenuItem
+            key={index}
+            className={menuItemClasses}
+            onClick={action.action}
+            shortcut="ctrl alt del"
+          >
+            <i className="w-2 opacity-50">{action.icon}</i>
 
-      <StandardPopover
+            <span className="ml-0.5">{action.label}</span>
+          </MenuItem>
+        ))}
+      </NavbarMenuDropdown>
+      {/* <StandardPopover
         trigger={
           <Button twClasses={`${baseClasses} px-1 rounded-l-none`}>
             <RiArrowDownSLine />
@@ -63,7 +80,7 @@ export function RunButton() {
             <span className="ml-0.5">{action.label}</span>
           </button>
         ))}
-      </StandardPopover>
+      </StandardPopover> */}
     </div>
   );
 }
--- frontend/src/components/Utils/atoms/menubar/menudropdown.jsx
+++ frontend/src/components/Utils/atoms/menubar/menudropdown.jsx
@@ -34,11 +34,17 @@ export function MenuDropdown({
   );
 }
 
-export function NavbarMenuDropdown({ children, trigger, tabIndex }) {
+export function NavbarMenuDropdown({
+  children,
+  trigger,
+  tabIndex,
+  triggerClassName = "nodrag flex cursor-pointer select-none items-center justify-between gap-1 rounded-md py-1 px-2 text-xs font-semibold leading-none text-setta-700 outline-none transition-colors duration-150 hover:bg-setta-100 focus-visible:ring-2 dark:text-setta-200 dark:hover:bg-setta-800",
+  contentClassName = "z-20 min-w-[200px] rounded-md border border-solid border-setta-100 bg-white p-1 shadow-xl transition-all will-change-[transform,opacity] dark:border-setta-700/50  dark:bg-setta-850",
+}) {
   return (
     <MenuDropdown
-      triggerClassName="nodrag flex cursor-pointer select-none items-center justify-between gap-1 rounded-md py-1 px-2 text-xs font-semibold leading-none text-setta-700 outline-none transition-colors duration-150 hover:bg-setta-100 focus-visible:ring-2 dark:text-setta-200 dark:hover:bg-setta-800"
-      contentClassName="z-20 min-w-[200px] rounded-md border border-solid border-setta-100 bg-white p-1 shadow-xl transition-all will-change-[transform,opacity] dark:border-setta-700/50  dark:bg-setta-850"
+      triggerClassName={triggerClassName}
+      contentClassName={contentClassName}
       contentAlign="start"
       contentSideOffset={5}
       contentAlignOffset={-3}
