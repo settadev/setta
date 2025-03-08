0eb67da5bb54087318f86e3082a905d336bf8816
Jeff Musgrave <jeff.musgrave@gmail.com>
2025-03-08 13:18:44 -0500

    added shortcut keys as placeholder for input in global search

--- frontend/src/components/AppHeader/SearchBar/ProjectPageSearch.jsx
+++ frontend/src/components/AppHeader/SearchBar/ProjectPageSearch.jsx
@@ -6,14 +6,20 @@ import {
 } from "components/Utils/Combobox/ComboboxParts";
 import { useIdNameCombobox } from "components/Utils/Combobox/useIdNameCombobox";
 import { getFloatingBoxHandlers } from "components/Utils/FloatingBox";
+import _ from "lodash";
 import React, { useRef } from "react";
 import { addSectionInEmptySpace } from "state/actions/sections/createSections";
 import { useCreateSectionsList } from "state/actions/sections/createSectionsHelper";
 import { goToSection } from "state/actions/sections/sectionPositions";
 import { maybeIncrementProjectStateVersion } from "state/actions/undo";
-import { useProjectSearch, useSectionInfos } from "state/definitions";
+import {
+  useProjectSearch,
+  useSectionInfos,
+  useSettings,
+} from "state/definitions";
 import { useUpdateProjectSearch } from "state/hooks/search";
 import { focusOnSection } from "utils/tabbingLogic";
+import { shortcutPrettified } from "utils/utils";
 
 export function ProjectPageSearchBar() {
   return (
@@ -38,13 +44,20 @@ function ProjectPageSearchBarWrapper({ children }) {
   }
 }
 
-const ProjectPageSearchInput = React.forwardRef((props, ref) => {
+function ProjectPageSearchInputComponent(props, ref) {
+  const shortcuts = useSettings((x) => {
+    return {
+      advancedkey: shortcutPrettified(x.shortcuts.advancedSearchShortcut),
+      commandkey: shortcutPrettified(x.shortcuts.commandPaletteShortcut),
+    };
+  }, _.isEqual);
+
   return (
     <StandardSearch
-      outerClasses="self-center w-[clamp(1rem,_30vw,_15rem)] ml-4"
-      inputStyles="flex-grow w-full cursor-auto overflow-hidden bg-setta-100 dark:bg-setta-950 border-setta-500 hover:bg-white outline-offset-2 focus:bg-white rounded-full text-xs text-setta-900 dark:text-setta-200 focus-visible:outline outline-blue-500 pl-7 py-1 placeholder-setta-300 dark:placeholder-setta-700"
+      outerClasses="self-center w-[clamp(1rem,_40vw,_40rem)] ml-4"
+      inputStyles="flex-grow w-full cursor-auto overflow-hidden bg-setta-100 dark:bg-setta-950 border border-solid border-setta-100/50 dark:border-setta-800 hover:bg-white outline-offset-2 focus:bg-white rounded-full text-xs text-setta-900 dark:text-setta-200 focus-visible:outline outline-blue-500 pl-7 py-1 placeholder-setta-300 dark:placeholder-setta-600"
       leftElementStyles="pl-1"
-      placeholder="Search Project"
+      placeholder={`Search (${shortcuts.advancedkey}) / Create (${shortcuts.commandkey})`}
       {...props}
       ref={ref}
       {...getFloatingBoxHandlers({
@@ -53,7 +66,10 @@ const ProjectPageSearchInput = React.forwardRef((props, ref) => {
       id="ProjectPageSearchBar"
     />
   );
-});
+}
+const ProjectPageSearchInput = React.forwardRef(
+  ProjectPageSearchInputComponent,
+);
 
 function ProjectPageFind({ children }) {
   const { value, onChange, onBlur, onFocus, onKeyDown } =
